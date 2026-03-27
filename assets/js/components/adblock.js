932c473aaae1218a55e30d6f50a417d02f26e6ad
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2026-03-27 18:40:17 +0100

    docs(jsdoc): replace `//` style comments with `/** */` JSDoc blocks in `adblock.js`
    Steps:
    - Convert 3 function comment blocks from `//` to `/** */` JSDoc format
    - Functions: `adblockDetected`, `adblockDisabled`, `adblockVerify`
    
    Reason:
    - JSDoc tools only recognize `/** */` blocks, not `//` comments
    - Consistency with the rest of the codebase
    
    References:
    - Issue #104

--- assets/js/components/adblock.js
+++ assets/js/components/adblock.js
@@ -1,11 +1,11 @@
 import { messageAlertAdd } from "./message.js";
 
-//
-// @function adblockDetected
-// @description Callback executed if adblock is installed
-// @see Used inside {@link messageAlert}
-// @see Used in: {@link checkAdblock}
-//
+/**
+ * @function adblockDetected
+ * @description Callback executed if adblock is installed
+ * @see Used inside {@link messageAlert}
+ * @see Used in: {@link checkAdblock}
+ */
 function adblockDetected() {
 	const message = "<i class='message-alert__icon icon-warning'></i> AdBlock is enabled";
 
@@ -14,22 +14,22 @@ function adblockDetected() {
 	messageAlertAdd("AdBlock", message);
 }
 
-//
-// @function adblockDisabled
-// @description Callback executed if adblock is disabled
-// @see Used in: {@link checkAdblock}
-//
+/**
+ * @function adblockDisabled
+ * @description Callback executed if adblock is disabled
+ * @see Used in: {@link checkAdblock}
+ */
 function adblockDisabled() {
 	// let message = "AdBlock is not enabled";
 	// console.info(message);
 }
 
-//
-// @function adblockVerify
-// @description Verify if the user has installed the Adblock browser extension
-// @see Used inside: {@link adblockDetected} {@link adblockDisabled}
-// @see Used in: {@link functionAnonimAutoExecuted}
-//
+/**
+ * @function adblockVerify
+ * @description Verify if the user has installed the Adblock browser extension
+ * @see Used inside: {@link adblockDetected} {@link adblockDisabled}
+ * @see Used in: {@link functionAnonimAutoExecuted}
+ */
 export function adblockVerify() {
 	if (typeof blockAdBlock === "undefined") {
 		adblockDetected();
